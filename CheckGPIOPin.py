cf3f6f40a3ea2323e1bbcf3b4342b8a7133be917
Luke Opperman <luke@opperman.net>
2025-11-05 10:27:17 -0500

    all setup in loop

--- CheckGPIOPin.py
+++ CheckGPIOPin.py
@@ -2,8 +2,6 @@
 import RPi.GPIO as GPIO
 import time
 
-# Set pin numbering mode (BCM or BOARD)
-GPIO.setmode(GPIO.BCM)
 
 # Define GPIO pin for checking
 off_pin = 16
@@ -12,20 +10,21 @@ debug_pin = 12
 # Function to check for connection to ground
 def pin_connected_to_ground(pin):
   for i in range(4):
-      # Retry loop for caught errors only
-      try:
-          GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-          # Read the pin value
-          pin_value = GPIO.input(pin)
-
-          # If pin value is LOW (0), then it's connected to ground
-          return pin_value == 0
-      except GPIO.lgpio.error as e:
-        time.sleep(0.5)
-      finally:
-        GPIO.cleanup()
-
+    # Retry loop for caught errors only
+    try:
+      GPIO.setmode(GPIO.BCM)
+      GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+      
+      # Read the pin value
+      pin_value = GPIO.input(pin)
+      
+      # If pin value is LOW (0), then it's connected to ground
+      return pin_value == 0
+    except GPIO.lgpio.error as e:
+      time.sleep(0.5)
+    finally:
+      GPIO.cleanup()
+      
 def mode():
   mode= "ACTIVE" # possible modes are OFF or DEBUG or ACTIVE
   if pin_connected_to_ground(debug_pin):
